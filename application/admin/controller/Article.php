b4ffef0e9d375904afdce7b332d36d5104e7e3c4
eastcityd <347374048@qq.com>
2020-05-29 18:08:36 +0800

    文章管理更新

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -4,25 +4,42 @@ namespace app\admin\controller;
 
 use app\common\controller\Base;
 use app\admin\model\Article as M;
+use think\View;
 
 class Article extends Base
 {
     private $m;
+    protected $view;
 
     public function __construct()
     {
         parent::__construct();
 
         $this->m = new M();
+        $this->view = new View();
     }
 
     public function index()
     {
+        $list = $this->m->getList();
+        $this->view->assign('list', $list);
         return $this->fetch();
     }
 
     public function type()
     {
-        return$this->fetch();
+        return $this->fetch();
+    }
+
+    public function addType()
+    {
+        if (request()->isPost()) {
+            if ($this->m->addType()) {
+                $this->success('添加成功', 'index');
+            } else {
+                $this->error($this->m->getError());
+            }
+        }
+        return $this->fetch();
     }
 }
--- application/admin/model/Article.php
+++ application/admin/model/Article.php
@@ -4,19 +4,65 @@ namespace app\admin\model;
 
 use think\Db;
 use think\Model;
+use think\Request;
+use think\Validate;
 
 class Article extends Model
 {
     //查询文章类型列表
     public function getList()
     {
-        $list = Db::name('article_type');
+        $list = Db::name('article_type')
+            ->order('id')
+            ->select();
+        return $list;
     }
 
     //增加文章类型
     public function addType()
     {
+        $request = Request::instance();
+        $data = $request->param();
 
+        $validate = new Validate(
+            [
+                'type_name' => 'require',
+                'type_description' => 'require'
+            ],
+            [
+                'type_name.require' => '请输入类型名称',
+                'type_description.require' => '请输入类型描述'
+            ]
+        );
+
+        if (!$validate->check($data)) {
+            $this->error = $validate->getError();
+            return false;
+        }
+
+        //判断类型名称是否已存在
+        $type_name = Db::name('article_type')
+            ->where('type_name', '=', $data['type_name'])
+            ->value('type_name');
+        if ($type_name) {
+            $this->error = '类型名称已存在';
+            return false;
+        }
+
+        $insert = array();
+        $insert['type_name'] = $data['type_name'] ?? '';
+        $insert['type_description'] = $data['type_description'] ?? '';
+        $insert['sort'] = $data['sort'] ?? 0;
+        $insert['status'] = $data['status'] ?? 1;
+
+        $res = Db::name('article_type')->insert($insert);
+
+        if (!$res) {
+            $this->error = '添加失败';
+            return false;
+        }
+
+        return true;
     }
 
     //更新文章类型
